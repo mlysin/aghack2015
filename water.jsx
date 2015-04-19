a47cd91baf213987fc8338f51e2291ef24871329
sin <emilybsin@gmail.com>
2015-04-18 21:11:13 -0700

    some more shit

--- water.jsx
+++ water.jsx
@@ -152,7 +152,7 @@ var PageLayout = React.createClass({
         var longitude = station ? station.longitude : "30";
         return (
             <div>
-                <h1>WaterLog™</h1><h2>Determine Your Evapotranspiration (ET) Rate</h2>
+                <center><h1>WaterLog™</h1><h2>Determine Your Evapotranspiration (ET) Rate</h2></center>
                 <Map latitude={latitude} longitude={longitude} /><br/>
                 <form className="form-horizontal" name="noname">
                     <fieldset style={{textAlign:'center'}}>
